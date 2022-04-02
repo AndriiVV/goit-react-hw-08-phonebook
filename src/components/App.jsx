9dfffbf2f89fb5ed445780b2ff0212ef8f74c570
Andrii Vinnykov <andrey.vinnikov@gmail.com>
2022-04-02 13:19:10 +0300

    Small commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   useEffect(() => {
     console.log("useEffect is running...");
     isAuth && dispatch(getCurUser());
-  }, [dispatch]);
+  }, [dispatch, isAuth]);
 
   return (
     <div>
